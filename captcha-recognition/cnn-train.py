52908fcf7684150d670011c887e88fdc205d5280
Dmitry <ermolaev@cvisionlab.com>
2017-05-23 19:42:06 +0300

    Finally fix

--- captcha-recognition/cnn-train.py
+++ captcha-recognition/cnn-train.py
@@ -81,9 +81,8 @@ def train(app_args):
         actual = tf.argmax(labels, 2)
         equal = tf.equal(tf.cast(prediction, tf.int32),
                          tf.cast(actual, tf.int32))
-        summ = tf.reduce_sum(tf.cast(equal, tf.int32), 1)
-        summ = tf.divide(summ, app_args.batch_size)
-        accuracy = tf.reduce_mean(tf.cast(equal, tf.float32), name="accuracy")
+        accuracy = tf.reduce_sum(tf.cast(equal, tf.int32), 1)
+        accuracy = tf.divide(accuracy, CAPTCHA_SIZE, name="accuracy")
         init_op = tf.initialize_all_variables()
 
         tf.summary.scalar("Loss", loss)
@@ -105,7 +104,7 @@ def train(app_args):
                         trace_level=tf.RunOptions.FULL_TRACE)
                     run_metadata = tf.RunMetadata()
                     _, loss_value, precission, summary = session.run(
-                        [train_op, loss, summ, summary_op],
+                        [train_op, loss, accuracy, summary_op],
                         options=run_options,
                         run_metadata=run_metadata)
 
